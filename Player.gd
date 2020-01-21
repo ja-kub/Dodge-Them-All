291c0e33161cefe9c5087a91834962834a9673b4
ja-kub <Ptaszor1!>
2020-01-21 21:56:25 +0100

    initial commit

---
+++ Player.gd
@@ -0,0 +1,71 @@
+extends Area2D
+
+signal hit
+
+export var speed = 400  # How fast the player will move (pixels/sec).
+var velocity = Vector2()
+var screen_size  # Size of the game window.
+var target = Vector2() # Variable to hold the clicked position.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	hide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# Move towards the target and stop when close.
+	if position.distance_to(target) > 10:
+		velocity = (target - position).normalized() * speed
+#		print(velocity)
+	else:
+		velocity = Vector2()
+
+# Remove keyboard controls.
+#    if Input.is_action_pressed("ui_right"):
+#       velocity.x += 1
+#    if Input.is_action_pressed("ui_left"):
+#        velocity.x -= 1
+#    if Input.is_action_pressed("ui_down"):
+#        velocity.y += 1
+#    if Input.is_action_pressed("ui_up"):
+#        velocity.y -= 1
+
+	if velocity.length() > 0:
+#		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+
+	position += velocity * delta
+#	print(position)
+	# We still need to clamp the player's position here because on devices that don't
+	# match your game's aspect ratio, Godot will try to maintain it as much as possible
+	# by creating black borders, if necessary.
+	# Without clamp(), the player would be able to move under those borders.
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "right"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
+
+func _on_Player_body_entered(body):
+	hide()  # Player disappears after being hit.
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	target = pos # Initial target is the start position.
+	show()
+	$CollisionShape2D.disabled = false
+
+# Change the target whenever a touch event happens.
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		target = event.position
